e5a0dd4aed58509d574cc5802bc831129699e1a4
Siddheshwar Ghuge <siddheshwarg4u@gmail.com>
2023-06-02 12:48:02 +0530

    First

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#multi",{
+    strings: [,  "Electronics Engineer !","Problem Solver !","Web Developer !"] ,
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+
+    })
