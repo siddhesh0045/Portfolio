2f8913c0d18fc7424b2d4801206a9d4fc1250d4d
Siddheshwar Ghuge <siddheshwarg4u@gmail.com>
2023-06-06 11:26:01 +0530

    Updated project with new files and modifications

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick =()=>{
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+// remove once clicked
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
+
 var typed = new Typed("#multi",{
-    strings: [,  "Electronics Engineer !","Problem Solver !","Web Developer !"] ,
-    typeSpeed: 100,
+    strings: [,  "Student !","Problem Solver !","Web Developer !"] ,
+    typeSpeed: 50,
     backSpeed: 100,
     loop: true
 
     })
+    
+
+
+  
